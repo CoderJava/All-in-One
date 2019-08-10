1e418e682cb1aa274ded38f73bd5d3f53164b532
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-08-10 09:10:20 +0700

    Setup pom.xml file

---
+++ src/main/kotlin/com/ysn/allinone/AllinoneApplication.kt
@@ -0,0 +1,11 @@
+package com.ysn.allinone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllinoneApplication
+
+fun main(args: Array<String>) {
+    runApplication<AllinoneApplication>(*args)
+}
